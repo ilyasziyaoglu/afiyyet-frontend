21129af6ee1cb89e078db3e2eb014997698cf0b3
Emil Mammadov <emilmammadov303@gmail.com>
2020-09-29 01:29:02 +0300

    time-zone problem solved

--- src/app/pages/admin/campaign-edit/campaign-edit.component.ts
+++ src/app/pages/admin/campaign-edit/campaign-edit.component.ts
@@ -21,6 +21,8 @@ export class CampaignEditComponent implements OnInit {
 
     this.status = this.router.getCurrentNavigation().extras.state.status;
     this.campaign = this.router.getCurrentNavigation().extras.state.data.item || {};
+    this.campaign.startDate = this.dateToISOString(this.campaign.startDate)
+    this.campaign.expireDate = this.dateToISOString(this.campaign.expireDate);
   }
 
   ngOnInit(): void {
@@ -77,5 +79,11 @@ export class CampaignEditComponent implements OnInit {
     console.log("image", image.files[0]);
     this.formData = new FormData();
     this.formData.append('file0', image.files[0]);
+  }
+
+  dateToISOString(str) {
+    let date = new Date(str);
+    return new Date(date.getTime() - (date.getTimezoneOffset() * 60000))
+        .toISOString().slice(0, -1);
   }
 }
