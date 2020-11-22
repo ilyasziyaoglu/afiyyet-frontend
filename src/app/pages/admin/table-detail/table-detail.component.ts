24117bbab681c5bea443632df83560e78fe67de7
ILYAS ZIYAOGLU <ilyasziyaoglu@gmail.com>
2020-11-22 17:19:00 +0300

    admin sipariş güncelleme tamamlandı

--- src/app/pages/admin/table-detail/table-detail.component.ts
+++ src/app/pages/admin/table-detail/table-detail.component.ts
@@ -8,6 +8,7 @@ import {OrderService} from '../../../services/order.service';
 import Swal from 'sweetalert2';
 import {OrderItemService} from '../../../services/order-item.service';
 import {Router} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
     selector: 'app-table-detail',
@@ -18,9 +19,12 @@ export class TableDetailComponent implements OnInit {
     filteredProducts: Observable<Product[]>;
 
     displayedColumns: string[] = ['position', 'name', 'amount', 'portion', 'price', 'totalPrice', 'state', 'comment', 'actions'];
+    editMode = false;
+    currentOrderItem: OrderItem;
 
     constructor(
         private router: Router,
+        public location: Location,
         public tableService: TableService,
         private orderService: OrderService,
         private orderItemService: OrderItemService,
@@ -51,18 +55,40 @@ export class TableDetailComponent implements OnInit {
 
     onAddOrder() {
         if ( this.addOrderForm.valid ) {
-            const order = {
-                tableId: this.tableService.currentTable.id,
-                orderItems: [this.addOrderForm.value],
-            };
-            this.orderService.createOrder(order, res => {
-                if ( res ) {
-                    this.tableService.currentTable.order = res;
-                    Swal.fire('Sonuç', 'Sipariş başarı ile eklendi.', 'success');
-                } else {
-                    Swal.fire('Hata', 'Sipariş eklerken bir hata oluştu!', 'error');
-                }
-            });
+
+            if ( this.editMode ) {
+                const orderItemRequest = {
+                    id: this.currentOrderItem.id,
+                    orderId: this.currentOrderItem.orderId,
+                    amount: this.addOrderForm.controls.amount.value,
+                    portion: this.addOrderForm.controls.portion.value,
+                    comment: this.addOrderForm.controls.comment.value,
+                };
+                this.orderItemService.put(orderItemRequest, res => {
+                    this.editMode = false;
+                    if ( res ) {
+                        this.tableService.updateCurrentTable();
+                        this.resetForm();
+                        Swal.fire('Sonuç', 'Sipariş başarı ile güncellendi.', 'success');
+                    } else {
+                        Swal.fire('Hata', 'Sipariş güncellenirken bir hata oluştu!', 'error');
+                    }
+                });
+            } else {
+                const order = {
+                    tableId: this.tableService.currentTable.id,
+                    orderItems: [this.addOrderForm.value],
+                };
+                this.orderService.createOrder(order, res => {
+                    if ( res ) {
+                        this.tableService.currentTable.order = res;
+                        this.resetForm();
+                        Swal.fire('Sonuç', 'Sipariş başarı ile eklendi.', 'success');
+                    } else {
+                        Swal.fire('Hata', 'Sipariş eklerken bir hata oluştu!', 'error');
+                    }
+                });
+            }
         }
     }
 
@@ -71,7 +97,6 @@ export class TableDetailComponent implements OnInit {
     }
 
     async onRemoveOrder(orderItem: OrderItem) {
-
         const {value: cancelForm} = await Swal.fire({
             title: 'İptal',
             showCancelButton: true,
@@ -79,8 +104,8 @@ export class TableDetailComponent implements OnInit {
             cancelButtonText: 'İptal',
             showLoaderOnConfirm: true,
             html:
-                '<input matInput id="cancel-amount" placeholder="İptal adedi" class="swal2-input">' +
-                '<input matInput id="cancel-reason" placeholder="İptal nedeni" class="swal2-input">',
+                '<input matInput id="cancel-amount" required value="' + orderItem.amount + '" placeholder="İptal adedi" class="swal2-input">' +
+                '<input matInput id="cancel-reason" required placeholder="İptal nedeni" class="swal2-input">',
             focusConfirm: false,
             preConfirm: () => {
                 return {
@@ -111,18 +136,17 @@ export class TableDetailComponent implements OnInit {
             cancelButtonText: 'İptal',
             showLoaderOnConfirm: true,
             html:
-                '<input matInput id="discount-amount" value="0" placeholder="İndirim miktarı" class="swal2-input">' +
+                '<input matInput id="discount-amount" type="number" value="0" placeholder="İndirim miktarı" class="swal2-input">' +
                 '<div class="custom-control custom-switch">\n' +
                 '  <input type="checkbox" class="custom-control-input" id="is-percent">\n' +
                 '  <label class="custom-control-label" for="is-percent">Yüzde</label>\n' +
                 '</div>',
             focusConfirm: false,
             preConfirm: () => {
-                console.log((document.getElementById('is-percent') as HTMLInputElement).value);
                 return {
                     id: this.tableService.currentTable.id,
-                    discountAmount: (document.getElementById('discount-amount') as HTMLInputElement).value,
-                    isPercent: (document.getElementById('is-percent') as HTMLInputElement).value,
+                    discountAmount: parseInt((document.getElementById('discount-amount') as HTMLInputElement).value, 10),
+                    isPercent: (document.getElementById('is-percent') as HTMLInputElement).checked,
                 };
             },
         });
@@ -137,5 +161,23 @@ export class TableDetailComponent implements OnInit {
                 }
             });
         }
+    }
+
+    onEditOrder(element: any) {
+        this.editMode = true;
+        this.addOrderForm.controls.amount.setValue(element.amount);
+        this.addOrderForm.controls.portion.setValue(element.portion.toString());
+        this.addOrderForm.controls.comment.setValue(element.comment);
+        this.addOrderForm.controls.productSearch.setValue(element.product.name);
+        this.addOrderForm.controls.product.setValue(element.product);
+        this.currentOrderItem = element;
+    }
+
+    resetForm() {
+        this.addOrderForm.controls.product.reset();
+        this.addOrderForm.controls.productSearch.reset();
+        this.addOrderForm.controls.amount.reset(1);
+        this.addOrderForm.controls.portion.reset('1');
+        this.addOrderForm.controls.comment.reset();
     }
 }
